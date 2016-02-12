f84a198dfb12789c38d72fdf844503cddbb031c2
marcherrera12 <marcherrera10@gmail.com>
2016-02-12 19:53:51 +0100

    serial 1.3

---
+++ serial1.3/serial1.3.ino
@@ -0,0 +1,23 @@
+/********************************************************************************
+**    El programa Serial 1.3 serveix perque l'arduino es comuniqui amb tu.     **
+**    L'arduino ho fa amb el monitor del programa.                             **
+**    En aquest cas ho escriu cada 1 segon amb salt de linia.                **
+********************************************************************************/
+
+//*******  Includes  ************************************************************
+
+
+//*******  Variables  ***********************************************************
+
+
+// La funció Setup només s'executa un cop. Quan la placa s'encen o es fa reset.
+void setup()
+{
+  Serial.begin(9600);       // Engega la llibreria serial a la velocitat 9600 bps
+}
+
+// La funció Loop es va repetin cada cop.
+void loop() {
+  Serial.println("Hello world!"); // Escriure amb un salt de linia
+  delay(1000); // Espera 1s = 1000 ms
+}
