f5caa42c4fbcd2f98bbff2a4053787bc1a519b63
marcherrera12 <marcherrera10@gmail.com>
2016-02-15 16:14:24 +0100

    Serial 2.3

---
+++ serial2.3/serial2.3.ino
@@ -0,0 +1,35 @@
+/********************************************************************************
+**    El programa Serial 2.3 serveix perque l'arduino es comuniqui amb tu.     **
+**    L'arduino ho fa amb el monitor del programa.                             **
+**    Converteix els GB a MB.                                                  **
+********************************************************************************/
+
+//*******  Includes  ************************************************************
+
+
+//*******  Variables  ***********************************************************
+
+int drive_gb = 5;
+int drive_mb;
+
+// La funció Setup només s'executa un cop. Quan la placa s'encen o es fa reset.
+void setup()
+{
+  Serial.begin(9600);       // Engega la llibreria serial a la velocitat 9600 bps
+
+  Serial.print("Your HD is ");
+  Serial.print(drive_gb);
+  Serial.println(" GB large.");
+
+  drive_mb = 1024 * drive_gb;  // Equivalencia dels MB amb GB.
+
+  Serial.print("It can store ");
+  Serial.print(drive_mb);
+  Serial.println(" Megabytes!");
+}
+
+
+// La funció Loop es va repetin cada cop.
+void loop() {
+//En aquest cas no fa res
+ }
